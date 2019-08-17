5d7946cea8f924ccff6f2b8e2cec276c7bde5d02
samael042 <44134245+samael042@users.noreply.github.com>
2019-08-17 10:09:34 +0530

    Add files via upload

---
+++ floydwarshall.cpp
@@ -0,0 +1,20 @@
+const int MAXN = 1e3+5;
+int d[N];
+//set d[i][j] = w(i,j) initially, if there is an edge btw i and j.
+//set it as oo otherwise
+//d[i][i] = 0 for undirected graphs
+void warshall()
+{
+	for(int i = 0; i < n; i++)
+		dp[i][i] = 0;
+	for(int k = 0; k < n; k++)
+	{
+		for(int i = 0; i < n; i++)
+		{
+			for(int j = 0; j < n; j++)
+			{
+				d[i][j] = min(d[i][j],d[i][k]+d[k][j]);
+			}
+		}
+	}	
+}

b65f0a34985ab15b233b84c0c12536d90f103ef1
samael042 <44134245+samael042@users.noreply.github.com>
2020-01-20 21:02:37 +0530

    Update bigint.cpp

--- bigint.cpp
+++ bigint.cpp
@@ -1,170 +1,204 @@
-struct bigint{
-  static const int LEN = 60;
-  static const int BIGMOD = 10000;
-  int s;			// sign of big integer
-  int vl, v[LEN];	// vl is length of v array
-  bigint() : s(1) { vl = 0; }   // eg. bigint x;
+const int bignumlen=2200; //高精度的位数 
+const int Blen=8; //压几位 
+const int64 base=100000000; //对应的基数 
 
-  bigint(long long a) {			// eg. bigint x(a);
-	s = 1; vl = 0;
-	if (a < 0) { s = -1; a = -a; }
-	while (a) {
-	  push_back(a % BIGMOD);
-	  a /= BIGMOD;
+struct bignum{
+	int len;
+	int64 data[bignumlen];
+	int64 &operator [](int x){ return(data[x]);}
+	const int64 &operator [](int x)const { return(data[x]);}
+	bignum (){
+		memset(data,0,sizeof(data));
+		len=0;
 	}
-  }
-
-  bigint(string str) {			// eg. bigint x(str);
-	s = 1; vl = 0;
-	int stPos = 0, num = 0;
-	if (!str.empty() && str[0] == '-') {
-	  stPos = 1;
-	  s = -1;
+	void clear(){
+		for(int i=len;i>=1;--i)data[i]=0;
+		len=0;
 	}
-	for (int i=str.length()-1, q=1; i>=stPos; i--) {
-	  num += (str[i] - '0') * q;
-	  if ((q *= 10) >= BIGMOD) {
-		push_back();
-		num = 0; q = 1;
-	  }
-	}
-	if (num) push_back(num);
-  }
-  int len() const {
-	return vl;
-  }
-
-  bool empty() const { return len() == 0; }
-
-  void push_back(int x) {
-	v[vl++] = x;
-  }
-
-  void pop_back() {
-	vl--;
-  }
-
-  int back() const {
-	return v[vl-1];
-  }
-
-  void n() {
-	while (!empty() && !back()) pop_back();
-  }
-
-  void resize(int nl) {
-	vl = nl;
-	memset(v,0,sizeof(int)*vl);
-  }
-  void print() const {
-	if (empty()) { putchar('0'); return; }
-	if (s == -1) putchar('-');
-	printf("%d", back());
-	for (int i=len()-2; i>=0; i--) printf("%.4d",v[i]);
-  }
-
-  friend std::ostream& operator << (std::ostream& out, const bigint &a) {
-	if (a.empty()) { out << "0"; return out; }
-	if (a.s == -1) out << "-";
-	out << a.back();
-	for (int i=a.len()-2; i>=0; i--) {
-	  char str[10];
-	  snprintf(str, 5, "%.4d", a.v[i]);
-	  out << str;
-	}
-	return out;
-  }
-  int compare(const bigint &b)const {
-	if (s != b.s) return s - b.s;
-	if (s == -1) return -(-*this).compare(-b);
-	if (len() != b.len()) return len()-b.len();//int
-	for (int i=len()-1; i>=0; i--)
-	  if (v[i]!=b.v[i]) return v[i]-b.v[i];
-	return 0;
-  }
-  bool operator < (const bigint &b)const{ return compare(b)<0; }
-  bool operator <= (const bigint &b)const{ return compare(b)<=0; }
-  bool operator == (const bigint &b)const{ return compare(b)==0; }
-  bool operator != (const bigint &b)const{ return compare(b)!=0; }
-  bool operator > (const bigint &b)const{ return compare(b)>0; }
-  bool operator >= (const bigint &b)const{ return compare(b)>=0; }
-  bigint operator - () const {
-	bigint r = (*this);
-	r.s = -r.s;
-	return r;
-  }
-  bigint operator + (const bigint &b) const {
-	if (s == -1) return -(-(*this)+(-b));
-	if (b.s == -1) return (*this)-(-b);
-	bigint r;
-	int nl = max(len(), b.len());
-	r.resize(nl + 1);
-	for (int i=0; i<nl; i++) {
-	  if (i < len()) r.v[i] += v[i];
-	  if (i < b.len()) r.v[i] += b.v[i];
-	  if(r.v[i] >= BIGMOD) {
-		r.v[i+1] += r.v[i] / BIGMOD;
-		r.v[i] %= BIGMOD;
-	  }
-	}
-	r.n();
-	return r;
-  }
-  bigint operator - (const bigint &b) const {
-	if (s == -1) return -(-(*this)-(-b));
-	if (b.s == -1) return (*this)+(-b);
-	if ((*this) < b) return -(b-(*this));
-	bigint r;
-	r.resize(len());
-	for (int i=0; i<len(); i++) {
-	  r.v[i] += v[i];
-	  if (i < b.len()) r.v[i] -= b.v[i];
-	  if (r.v[i] < 0) {
-		r.v[i] += BIGMOD;
-		r.v[i+1]--;
-	  }
-	}
-	r.n();
-	return r;
-  }
-  bigint operator * (const bigint &b) {
-	bigint r;
-	r.resize(len() + b.len() + 1);
-	r.s = s * b.s;
-	for (int i=0; i<len(); i++) {
-	  for (int j=0; j<b.len(); j++) {
-		r.v[i+j] += v[i] * b.v[j];
-		if(r.v[i+j] >= BIGMOD) {
-		  r.v[i+j+1] += r.v[i+j] / BIGMOD;
-		  r.v[i+j] %= BIGMOD;
+	int check (const bignum &a,const bignum &b){
+		if(a.len>b.len)return(0);
+		if(b.len>a.len)return(1);
+		for(int i=a.len;i>=1;--i){
+			if(a.data[i]<b.data[i])return(1);
+			if(b.data[i]<a.data[i])return(0);
 		}
-	  }
+		return 2;
 	}
-	r.n();
-	return r;
-  }
-  bigint operator / (const bigint &b) {
-	bigint r;
-	r.resize(max(1, len()-b.len()+1));
-	int oriS = s;
-	bigint b2 = b; // b2 = abs(b)
-	s = b2.s = r.s = 1;
-	for (int i=r.len()-1; i>=0; i--) {
-	  int d=0, u=BIGMOD-1;
-	  while(d<u) {
-		int m = (d+u+1)>>1;
-		r.v[i] = m;
-		if((r*b2) > (*this)) u = m-1;
-		else d = m;
-	  }
-	  r.v[i] = d;
+	bool operator < (const bignum &b){ return(check(*this,b)==1);}
+	bool operator > (const bignum &b){ return(check(*this,b)==0);}
+	bool operator <=(const bignum &b){ return(check(*this,b)>=1);}
+	bool operator >=(const bignum &b){ return(check(*this,b)%2==0);}
+	bool operator !=(const bignum &b){ return(check(*this,b)!=2);}
+	bool operator ==(const bignum &b){ return(check(*this,b)==2);}
+	
+	bignum operator=(const bignum &x){
+		for(int i=x.len+1;i<=len;++i)data[i]=0;
+		for(int i=1;i<=x.len;++i)data[i]=x.data[i];
+		len=x.len;
+		return *this;
 	}
-	s = oriS;
-	r.s = s * b.s;
-	r.n();
-	return r;
-  }
-  bigint operator % (const bigint &b) {
-	return (*this)-(*this)/b*b;
-  }
+	bignum operator=(int64 x){
+		for(int i=len;i>=0;--i)data[i]=0;
+		len=0;
+		while(x){
+			data[++len]=x%base;
+			x/=base;
+		}
+		return *this;
+	}
+	bignum(int64 x){
+		memset(data,0,sizeof(data));
+		len=0;
+		(*this)=x;
+	}
+	bignum operator *(const bignum &b){
+		int i,j;
+		bignum tmp;
+		for(i=1;i<=len;++i)if(data[i]!=0)
+			for(j=1;j<=b.len;++j)if(b.data[j]!=0){
+				tmp.data[i+j-1]+=data[i]*b.data[j];
+				tmp.data[i+j]+=tmp.data[i+j-1]/base;
+				tmp.data[i+j-1]%=base;
+			}
+		tmp.len=len+b.len-1;
+		while(tmp.data[tmp.len+1])tmp.len++;
+		return tmp;
+	}
+	bignum operator *(int64 x){
+		int i;
+		bignum tmp;
+		for(i=1;i<=len;++i)tmp[i]=data[i]*x;
+		tmp.len=len;
+		for(i=1;i<=len;++i){
+			tmp[i+1]+=tmp[i]/base,tmp[i]%=base;
+			if(tmp[i+1]&&i+1>tmp.len)tmp.len++;
+		}
+		return tmp;
+	}
+	bignum operator /(int64 x){
+		int i;
+		bignum tmp;
+		int64 y=0;
+		for(i=len;i>=1;--i){
+			y=y*base+data[i];
+			tmp[i]=y/x;
+			y%=x;
+		}
+		tmp.len=len;
+		while(tmp[tmp.len]==0&&tmp.len>1)tmp.len--;
+		return tmp;
+	}
+	bignum operator /(const bignum &b){
+		if(b.len<=1 && b[1]==0){
+			printf("error! 被0除!");
+			for(;;);
+		}
+		int i,l1=(len-1)*Blen,l2=(b.len-1)*Blen;
+		int64 x=data[len],y=b[b.len];
+		while(x)x/=10,l1++;
+		while(y)y/=10,l2++;
+		bignum tmp,chu,B;
+		chu=*this; B=b;
+		
+		for(i=1;i*Blen<=l1-l2;++i)B*=base;
+		for(i=1;i<=(l1-l2)%Blen;++i)B*=10;
+		for(i=l1-l2;i>=0;--i){
+			x=0;
+			while(chu>=B)chu-=B,x++;
+			tmp[i/Blen+1]=tmp[i/Blen+1]*10+x;
+			B/=10;
+		}
+		tmp.len=(l1-l2)/Blen+1;
+		while(tmp.len>=1 && !tmp[tmp.len])tmp.len--;
+		return tmp;
+	}
+	bignum operator +(const bignum &b){
+		bignum tmp;
+		int i,l=max(len,b.len);
+		for(i=1;i<=l;++i)tmp[i]=data[i]+b[i];
+		for(i=1;i<=l;++i)tmp[i+1]+=tmp[i]/base,tmp[i]%=base;
+		tmp.len=l;
+		if(tmp[tmp.len+1])tmp.len++;
+		return tmp;
+	}
+	bignum operator +(int64 x){
+		bignum tmp; tmp=*this;
+		tmp[1]+=x;
+		for(int i=1;i<=len&&tmp[i]>=base;++i)tmp[i+1]+=tmp[i]/base,tmp[i]%=base;
+		while(tmp[tmp.len+1])tmp.len++;
+		return tmp;
+	}
+	bignum operator -(const bignum &b){
+		int i;
+		bignum tmp;
+		for(i=1;i<=len;++i)tmp.data[i]=data[i]-b.data[i];
+		for(i=1;i<=len;++i){
+			if(tmp[i]<0)tmp.data[i]+=base,tmp.data[i+1]--;
+		}
+		tmp.len=len;
+		while(tmp[tmp.len]==0&&tmp.len>1)tmp.len--;
+		return tmp;
+	}
+	bignum operator -(int64 x){
+		bignum tmp; tmp=*this;
+		tmp[1]-=x;
+		for(int i=1;i<=len&&tmp[i]<0;++i){
+			tmp[i+1]+=(tmp[i]+1)/base-1;
+			tmp[i]=(tmp[i]+1)%base+base-1;
+		}
+		while(!tmp[tmp.len]&&tmp.len>1)tmp.len--;
+		return tmp;
+	}
+	int64 operator %(int64 x){
+		int i;
+		int64 y=0;
+		for(i=len;i>=1;--i)y=(y*base+data[i])%x;
+		return y;
+	}
+	bignum operator %(const bignum &b){
+		if(b.len<=1 && b[1]==0){
+			printf("error! 被0 mod!");
+			for(;;);
+		}
+		int i,l1=(len-1)*Blen,l2=(b.len-1)*Blen;
+		int64 x=data[len],y=b[b.len];
+		while(x)x/=10,l1++;
+		while(y)y/=10,l2++;
+		bignum chu,B;
+		chu=*this; B=b;
+		
+		for(i=1;i*Blen<=l1-l2;++i)B*=base;
+		for(i=1;i<=(l1-l2)%Blen;++i)B*=10;
+		for(i=l1-l2;i>=0;--i){
+			while(chu>=B)chu-=B;
+			B/=10;
+		}
+		return chu;
+	}
+	
+	bignum operator +=(const bignum &b){return *this=(*this+b);}
+	bignum operator *=(const bignum &b){return *this=(*this*b);}
+	bignum operator -=(const bignum &b){return *this=(*this -b);}
+	bignum operator /=(const bignum &b){return *this=(*this/b);}
+	bignum operator %=(const bignum &b){return *this=(*this%b);}
+	bignum operator *=(int64 x) {return( *this=(*this *x));}
+	bignum operator +=(int64 x) {return( *this=(*this +x));}
+	bignum operator -=(int64 x) {return( *this=(*this -x));}
+	bignum operator /=(int64 x) {return( *this=(*this /x));}
+	void read(){
+		char c[bignumlen*Blen+10];
+		scanf("%s",c+1);
+		int l=strlen(c+1);
+		(*this).clear();
+		int64 x;
+		for(int i=1;i<=(l-1)/Blen+1;++i){
+			x=0;
+			for(int j=l-Blen*i+1;j<=l-Blen*i+Blen;++j)if(j>=1)x=x*10+c[j]-48;
+			data[++len]=x;
+		}
+	}
+	void write(){
+		printf("%I64d",data[len]);
+		for(int i=len-1;i>=1;--i)printf("%0*I64d",Blen,data[i]);
+	}
 };
